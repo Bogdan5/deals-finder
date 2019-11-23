abe91a2dad1925fc341a979c4602c321de2d8c24
Bogdan5 <bogdan5x@gmail.com>
2019-11-22 23:08:55 -0500

    Added passport to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const passport = require("passport");
 require('custom-env').env();
 
 const users = require('./routes/api/users');
@@ -22,6 +23,13 @@ app.use(morgan('tiny'));
 // routes
 app.use('/users', users);
 app.use('/test', test);
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
